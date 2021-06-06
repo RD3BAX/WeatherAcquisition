2875de1fe2b042bfc68340f1f99cd37d3c966c28
Magistr <MagistrTNT@msn.com>
2021-06-06 12:11:14 +0300

    Подготовка консольного приложения

--- Ul/WeatherAcquisition.ConsoleUI/Program.cs
+++ Ul/WeatherAcquisition.ConsoleUI/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WeatherAcquisition.ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+
+
+            Console.WriteLine("Завешено!");
+            Console.ReadLine();
+            await host.StartAsync();
         }
     }
 }
