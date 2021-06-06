cc6492f72a060cfa43682e043b4513294da511e5
Magistr <MagistrTNT@msn.com>
2021-06-06 14:50:33 +0300

    Подготовка контекста БД

---
+++ Data/WeatherAcquisition.DAL/Context/DataDB.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAcquisition.DAL.Entities;
+
+namespace WeatherAcquisition.DAL.Context
+{
+    class DataDB : DbContext
+    {
+        public DbSet<DataValue> Values { get; set; }
+
+        public DbSet<DataSource> Sources { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DataSource>()
+                .HasMany<DataValue>()
+                .WithOne(v => v.Source)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        #region Конструктор
+
+        public DataDB(DbContextOptions<DataDB> options) :base(options) { }
+
+        #endregion // Конструктор
+    }
+}
