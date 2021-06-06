22bc5e1671142c188148d8279cdf51e0452566d2
Magistr <MagistrTNT@msn.com>
2021-06-06 11:43:28 +0300

    Подготовка инфраструктуры WPF-приложения

---
+++ Ul/WeatherAcquisition.WPF/ServiceLocator.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeatherAcquisition.WPF.ViewModels;
+
+namespace WeatherAcquisition.WPF
+{
+    class ServiceLocator
+    {
+        public MainWindowViewModel MainModel => App.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
---
+++ Ul/WeatherAcquisition.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MathCore.WPF.ViewModels;
+
+namespace WeatherAcquisition.WPF.ViewModels
+{
+    class MainWindowViewModel : TitledViewModel
+    {
+        #region Конструктор
+
+        public MainWindowViewModel()
+        {
+            Title = "Главное окно программы";
+        }
+
+        #endregion // Конструктор
+    }
+}
