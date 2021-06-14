54949bd8e45fa03b5d3b9c4c5b1aa99cdb58d2a2
Magistr <rd3bax@gmail.com>
2021-06-14 12:03:21 +0300

    Интерфейс репозитория для именованных сущностей

---
+++ Services/WeatherAcquisition.Interfaces.Base/Repositories/INamedRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WeatherAcquisition.Interfaces.Base.Entities;
+
+namespace WeatherAcquisition.Interfaces.Base.Repositories
+{
+    public interface INamedRepository<T> : IRepository<T> where T : INamedEntity
+    {
+        /// <summary>
+        /// Проверяет наличие сущности с указанным именем
+        /// </summary>
+        /// <param name="Name">Имя сущности</param>
+        /// <param name="Cancel"></param>
+        /// <returns></returns>
+        Task<bool> ExistName(string Name, CancellationToken Cancel = default);
+
+        /// <summary>
+        /// Находит сущность с указанным именем
+        /// </summary>
+        /// <param name="Name">Имя сущности</param>
+        /// <param name="Cancel"></param>
+        /// <returns>Найденная сущность</returns>
+        Task<T> GetByName(string Name, CancellationToken Cancel = default);
+
+        /// <summary>
+        /// Удаляет сущность по указанному имени
+        /// </summary>
+        /// <param name="Name">Имя сущности</param>
+        /// <param name="Cancel"></param>
+        /// <returns>Удаленная сущность</returns>
+        Task<T> DeleteByName(string Name, CancellationToken Cancel = default);
+    }
+}
