2eb6619f991817db67fc9d317a3d08e888dd2f25
Magistr <rd3bax@gmail.com>
2021-06-15 15:08:46 +0300

    WebAPI для доступа к методу получения постраничного разбиения элементов репозитория

--- Services/WeatherAcquisition.API/Controllers/DataSourcesController.cs
+++ Services/WeatherAcquisition.API/Controllers/DataSourcesController.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WeatherAcquisition.DAL.Entities;
@@ -21,6 +24,32 @@ namespace WeatherAcquisition.API.Controllers
         [HttpGet("count")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
         public async Task<IActionResult> GetItemsCount() => Ok(await _Repository.GetCount());
+
+        [HttpGet("exist/id/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
+        public async Task<IActionResult> ExistId(int id) => await _Repository.ExistId(id) ? Ok(true) : NotFound(false);
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll() => Ok(await _Repository.GetAll());
+
+        [HttpGet("items[[{Skip:int}:{Count:int}]]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<DataSource>>> Get(int Skip, int Count) =>
+            Ok(await _Repository.Get(Skip, Count));
+
+        [HttpGet("page/{PageIndex:int}/{PageSize:int}")]
+        [HttpGet("page[[{PageIndex:int}/{PageSize:int}]]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IPage<DataSource>>> GetPage(int PageIndex, int PageSize)
+        {
+            var result = await _Repository.GetPage(PageIndex, PageSize);
+            return result.Items.Any()
+                ? Ok(result)
+                : NotFound(result);
+        }
 
         #endregion // Методы
 
